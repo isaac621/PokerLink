c44c0050d4d0c4f9222fb9622ea8b9470f1463bc
isaac621 <isaac00621@gmail.com>
2022-04-01 01:11:00 +0800

    /

---
+++ client/src/components/UserRelated/Verify.js
@@ -0,0 +1,98 @@
+import { Box, TextField, Button, Typography, Alert, IconButton, Collapse,  } from "@mui/material"
+import { useContext, useEffect, useState } from "react"
+import { Link, useNavigate,  } from "react-router-dom";
+import { serverHost } from "../../constant";
+import CloseIcon from '@mui/icons-material/Close';
+
+
+
+export const Verify = () =>{
+
+    const [confirmationCode, setConfirmationCode] = useState();
+    const navigate = useNavigate()
+
+    const [message, setMessage] = useState();
+    const [severity, setSeverity] = useState();
+    const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const handleConfirmation = async()=>{
+        setLoading(true);
+        const res = await fetch(`${serverHost}/auth/confirm/${confirmationCode}`)
+        
+        if(res.ok){
+            setSeverity('success')
+            setTimeout(()=>navigate('/login'),2000)
+        }
+        else{
+            setSeverity('warning')
+        }
+        const resMessage = await res.json().then(res=>res.message)
+        setMessage(resMessage)
+        setOpen(true);
+        setLoading(false);
+    }
+    
+
+    return(
+        <Box sx={Style.container}>
+            <Typography variant="h2" display="block" gutterBottom >
+                Account Verification
+            </Typography>
+            <Collapse in={open}>
+            <Alert
+                    severity={severity}
+                    action={
+                        <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"
+                        onClick={() => {
+                            setOpen(false);
+                        }}
+                        >
+                            <CloseIcon fontSize="inherit" />
+                        </IconButton>
+                    }
+                    sx={{ mb: 2 }}
+                >
+                    {message}
+                </Alert>
+            </Collapse>
+            <Collapse in={severity != 'success'}>
+                <Box sx={Style.formContainer}>
+                    <TextField sx={Style.formItem} required label="Confimation Code" onChange={(e)=>{setConfirmationCode(e.target.value)}} type="password"/>
+                    <Button sx={Style.formItem} variant="outlined" onClick={handleConfirmation} disabled={loading || !confirmationCode}>Verify</Button>
+                </Box>
+                <Typography variant="caption" display="block" >
+                    Verified? <Link to='/login'>Login</Link>
+                </Typography>
+            </Collapse>
+            <Collapse in={severity == 'success'}>
+                <Typography>
+                    You are directing to the login page.....
+                </Typography>
+            </Collapse>
+            
+        </Box>
+    )
+}
+
+const Style = {
+    container: {
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    formContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    formItem: {
+        my: 1,
+        autocomplete:"nope"
+    }
+}
